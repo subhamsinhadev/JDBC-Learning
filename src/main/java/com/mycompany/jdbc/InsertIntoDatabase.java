81cd36081b5ba5453b13a653488147a32cbe2c2b
Subham Kumar Sinha <subhamsinha9206@gmail.com>
2025-08-12 01:24:16 +0530

    now we can insert data into database

--- src/main/java/com/mycompany/jdbc/InsertIntoDatabase.java
+++ src/main/java/com/mycompany/jdbc/InsertIntoDatabase.java
@@ -1,8 +1,10 @@
 package com.mycompany.jdbc;
 
+import java.io.DataInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class InsertIntoDatabase{
     public static void main(String[] args) {
@@ -10,10 +12,29 @@ public class InsertIntoDatabase{
         String user = "root";
         String password = "StrongPass123";
         System.out.println("Connecting to database...");
+        DataInputStream dis=new DataInputStream(System.in);
         try {
+
+            System.out.println("Enter the id of the new employee");
+            String empid=dis.readLine();
+            System.out.println("Enter the name of the new employee");
+            String empname=dis.readLine();
+            System.out.println("Enter the email of the new employee");
+            String empmail=dis.readLine();
+
+
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn= DriverManager.getConnection(url,user,password);
             System.out.println("Connected to database...");
+
+            Statement stmt=conn.createStatement();
+            String insertquery="insert into emp (emp_id,emp_name,emp_mail) values("+empid+",'"+empname+"','"+empmail+"')";
+//            stmt.executeQuery(insertquery);
+            int rowsAffected=stmt.executeUpdate(insertquery);
+            if(rowsAffected==1){
+                System.out.println("Added Succeessfully");
+            }
+            conn.close();
 
 
 
